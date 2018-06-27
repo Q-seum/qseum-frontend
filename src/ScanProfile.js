4c71b247249c3797a9f8bfd31e819f2df7ff35be
hsw919 <hunterwoody@gmail.com>
2018-06-27 11:12:16 -0400

    have it so only admin can view users now

--- src/ScanProfile.js
+++ src/ScanProfile.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 import './App.css'
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import request from 'superagent'
 import { Title, Box, Field, Label, Control, Input, Button, Content } from 'bloomer'
 
@@ -67,37 +67,40 @@ class ScanProfile extends Component {
   }
 
   render () {
-    console.log(this.props)
-    return (
-      <div className='ScanProfile'>
-        <Box>
-          <Title>Check In {this.state.username}</Title>
-          <form onSubmit={this.handleSubmit}>
-            <Field>
-              <Label htmlFor='visitors'>Number of Visitors</Label>
-              <Control>
-                <Input type='number' name='visitors' onChange={this.handleChange} id='visitors' />
-              </Control>
-            </Field>
-            <Field>
-              <Control>
-                <Button type='submit'>Check In</Button>
-              </Control>
-            </Field>
-          </form>
-          <Content>
-            <h1>{this.state.username}'s profile</h1>
-            <div>Username: {this.state.username}</div>
-            <div>Email: {this.state.email}</div>
-            <div>Account: {this.state.account}</div>
-            <div>MembershipType: {this.state.membershipType}</div>
-            <div>Accommodations: {this.state.accommodations}</div>
-            <div>Date Joined: {this.state.joinData}</div>
-            <div>Membership Expiration Date: {this.state.expirationDate}</div>
-          </Content>
-        </Box>
-      </div>
-    )
+    if (this.props.admin === 'true') {
+      return (
+        <div className='ScanProfile'>
+          <Box>
+            <Title>Check In {this.state.username}</Title>
+            <form onSubmit={this.handleSubmit}>
+              <Field>
+                <Label htmlFor='visitors'>Number of Visitors</Label>
+                <Control>
+                  <Input type='number' name='visitors' onChange={this.handleChange} id='visitors' />
+                </Control>
+              </Field>
+              <Field>
+                <Control>
+                  <Button type='submit'>Check In</Button>
+                </Control>
+              </Field>
+            </form>
+            <Content>
+              <h1>{this.state.username}'s profile</h1>
+              <div>Username: {this.state.username}</div>
+              <div>Email: {this.state.email}</div>
+              <div>Account: {this.state.account}</div>
+              <div>MembershipType: {this.state.membershipType}</div>
+              <div>Accommodations: {this.state.accommodations}</div>
+              <div>Date Joined: {this.state.joinData}</div>
+              <div>Membership Expiration Date: {this.state.expirationDate}</div>
+            </Content>
+          </Box>
+        </div>
+      )
+    } else {
+      return <Redirect to={'/'} />
+    }
   }
 }
 
