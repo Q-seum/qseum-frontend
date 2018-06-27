ab32ceec9bc8450b7f09c052182fc50d6f2c5c4b
kcbobbe <kcbobbe@gmail.com>
2018-06-27 15:48:21 -0400

    tried to integrate logo into the site

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
 import { Navbar, NavbarItem, NavbarLink, NavbarStart, NavbarEnd } from 'bloomer'
+import logo from './logo3.png'
 
 class Header extends Component {
   render () {
     return (
       <Navbar className='Header'>
         <NavbarStart>
+          {/* <NavbarItem>
+            <img className='logo' src={logo} />
+          </NavbarItem> */}
           <NavbarItem className='navtitle'>
             <strong>Q-Seum</strong>
           </NavbarItem>
