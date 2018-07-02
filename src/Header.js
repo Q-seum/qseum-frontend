dbcdcf27a322714bb4694ba606497d7ec2eb4a67
kcbobbe <kcbobbe@gmail.com>
2018-07-02 17:01:54 -0400

    background styles/colors

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
-import { Navbar, NavbarItem, NavbarStart, NavbarEnd } from 'bloomer'
+import { Navbar, NavbarItem, NavbarStart, NavbarEnd, NavbarBurger, NavbarBrand, NavbarMenu } from 'bloomer'
 // import logo from './logo3.png'
 
 class Header extends Component {
+  constructor () {
+    super()
+    this.state = {
+      isActive: false,
+    }
+    this.onClickNav = this.onClickNav.bind(this)
+  }
+
+  onClickNav () {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
+
   render () {
     return (
       <Navbar className='Header'>
-        <NavbarStart>
+        <NavbarBrand>
+          <NavbarItem>
+            <img src={require('./logo-separated.png')} />
+          </NavbarItem>
+        
           {/* <NavbarItem>
             <img className='logo' src={logo} />
           </NavbarItem> */}
@@ -16,29 +34,36 @@ class Header extends Component {
             <strong>Q-Seum</strong>
           </NavbarItem>
           <NavbarItem className='tagline'>Hop through the lines</NavbarItem>
-        </NavbarStart>
-        {this.props.token && (
-          <NavbarEnd className='header-links'>
-            <Link to='/'>
-              {this.props.admin === 'true' ? (
-                <NavbarItem isHoverable>Home</NavbarItem>
-              ) : (
-                <NavbarItem isHoverable>Your QR Code</NavbarItem>
-              )}
-            </Link>
-            <Link to='/Map'>
-              <NavbarItem isHoverable>Museum Map</NavbarItem>
-            </Link>
-            <Link to='/profile'>
-              <NavbarItem isHoverable>Your Profile</NavbarItem>
-            </Link>
-            {this.props.admin !== 'true' && (
-              <Link to='/report-issue'>
-                <NavbarItem isHoverable>Report an issue</NavbarItem>
+        
+          <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+        </NavbarBrand>
+        <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
+          <NavbarStart>
+          </NavbarStart>
+          {this.props.token && (
+            <NavbarEnd className='header-links'>
+              <Link to='/'>
+                {this.props.admin === 'true' ? (
+                  <NavbarItem isHoverable>Home</NavbarItem>
+                ) : (
+                  <NavbarItem isHoverable>Your QR Code</NavbarItem>
+                )}
               </Link>
-            )}
-          </NavbarEnd>
-        )}
+              <Link to='/Map'>
+                <NavbarItem isHoverable>Museum Map</NavbarItem>
+              </Link>
+              <Link to='/profile'>
+                <NavbarItem isHoverable>Your Profile</NavbarItem>
+              </Link>
+              {this.props.admin !== 'true' && (
+                <Link to='/report-issue'>
+                  <NavbarItem isHoverable>Report an issue</NavbarItem>
+                </Link>
+              )}
+            </NavbarEnd>
+          
+          )}
+        </NavbarMenu>
       </Navbar>
     )
   }
