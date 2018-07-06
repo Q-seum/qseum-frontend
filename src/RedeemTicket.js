a36d2c670610652d3e77592e734213204b342165
hsw919 <hunterwoody@gmail.com>
2018-07-06 12:56:45 -0400

    have confirming a visit for redeem ticket working

--- src/RedeemTicket.js
+++ src/RedeemTicket.js
@@ -3,14 +3,47 @@
 import React, { Component } from 'react'
 import './App.css'
 import { Redirect } from 'react-router-dom'
-import { Box } from 'bloomer'
+import { Box, Title, Button } from 'bloomer'
 import request from 'superagent'
 // import logo from './logo3.png'
 
 class RedeemTicket extends Component {
+  constructor () {
+    super()
+    this.state = {
+      childTickets: '',
+      genTickets: '',
+      milTickets: '',
+      senTickets: '',
+      recipEmail: ''
+    }
+
+    this.confirmVisit = this.confirmVisit.bind(this)
+  }
   componentDidMount () {
     request
       .get(`https://secure-temple-21963.herokuapp.com/api/v1/tickets/${this.props.match.params.id}`)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          childTickets: res.body.data.childTickets,
+          genTickets: res.body.data.genTickets,
+          milTickets: res.body.data.milTickets,
+          senTickets: res.body.data.senTickets,
+          recipEmail: res.body.data.recip_email
+        })
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
+  }
+
+  confirmVisit (e) {
+    request
+      .patch(`https://secure-temple-21963.herokuapp.com/api/v1/tickets/${this.props.match.params.id}`)
+      .send({
+        redeemed: true
+      })
       .then(res => {
         console.log(res)
       })
@@ -22,7 +55,13 @@ class RedeemTicket extends Component {
       return (
         <div className='RedeemTicket'>
           <Box className='transparent-box'>
-            <h1>success</h1>
+            <Title>Guest Ticket</Title>
+            <div><strong>General Tickets: </strong>{this.state.genTickets ? (this.state.genTickets) : (0)}</div>
+            <div><strong>Children Tickets: </strong>{this.state.childTickets ? (this.state.childTickets) : (0)}</div>
+            <div><strong>Military Tickets: </strong>{this.state.milTickets ? (this.state.milTickets) : (0)}</div>
+            <div><strong>Senior Tickets: </strong>{this.state.senTickets ? (this.state.senTickets) : (0)}</div>
+            {/* <div><strong>Last Visit: </strong>{lastVisit}</div> */}
+            <Button onClick={this.confirmVisit}>Confirm Visit</Button>
           </Box>
         </div>
       )
